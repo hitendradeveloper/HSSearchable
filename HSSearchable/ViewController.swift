442f96de687945a97e0ee9632b0492378ebda024
Hitendra Solanki <hitendra.spaceo@gmail.com>
2017-08-28 09:48:07 +0530

    Initial Commit

---
+++ HSSearchable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HSSearchable
+//
+//  Created by SOTSYS216 on 28/08/17.
+//  Copyright © 2017 Hitendra iDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
